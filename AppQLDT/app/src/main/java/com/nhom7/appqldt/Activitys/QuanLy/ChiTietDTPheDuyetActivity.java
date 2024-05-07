951d78acbb2d98c6711aea43cdf96b58bd645d91
tuoitho <tuoithokhoc1414@gmail.com>
2024-05-07 21:29:58 +0700

    xemthanhviendetaicanpheduyet

--- AppQLDT/app/src/main/java/com/nhom7/appqldt/Activitys/QuanLy/ChiTietDTPheDuyetActivity.java
+++ AppQLDT/app/src/main/java/com/nhom7/appqldt/Activitys/QuanLy/ChiTietDTPheDuyetActivity.java
@@ -1,6 +1,7 @@
 package com.nhom7.appqldt.Activitys.QuanLy;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.nhom7.appqldt.R;
 
 public class ChiTietDTPheDuyetActivity extends AppCompatActivity {
+    Button btnXemThanhVien, btnPheDuyet, btnKhongPheDuyet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chi_tiet_dtphe_duyet);
-
+        btnXemThanhVien = findViewById(R.id.btnXemThanhVien);
+        btnPheDuyet = findViewById(R.id.btnPheDuyet);
+        btnKhongPheDuyet = findViewById(R.id.btnKhongPheDuyet);
+        btnXemThanhVien.setOnClickListener(v -> {
+            XemThanhVienDeTaiDialogFragment dialogFragment = new XemThanhVienDeTaiDialogFragment();
+            dialogFragment.show(getSupportFragmentManager(), "XemThanhVienDeTaiDialogFragment");
+        });
 
     }
 }
---
+++ AppQLDT/app/src/main/java/com/nhom7/appqldt/Activitys/QuanLy/ThanhVienDeTaiAdapter.java
@@ -0,0 +1,69 @@
+package com.nhom7.appqldt.Activitys.QuanLy;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nhom7.appqldt.Models.ThanhVienDeTai;
+import com.nhom7.appqldt.R;
+
+import java.util.List;
+
+public class ThanhVienDeTaiAdapter extends RecyclerView.Adapter<ThanhVienDeTaiAdapter.ViewHolder> {
+    List<ThanhVienDeTai> listTV;
+    Context context;
+
+    public ThanhVienDeTaiAdapter(List<ThanhVienDeTai> listTV, Context context) {
+        this.listTV = listTV;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ThanhVienDeTaiAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_thanhvien_item_dialogfragment, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ThanhVienDeTaiAdapter.ViewHolder holder, int position) {
+        ThanhVienDeTai tv = listTV.get(position);
+        holder.tvStt.setText(String.valueOf(position+1));
+        holder.tvHoTen.setText(tv.getHoTen());
+        holder.tvMSSV.setText(tv.getMSSV());
+        holder.tvLop.setText(tv.getLop());
+        holder.tvKhoa.setText(tv.getKhoa());
+        holder.tvNgayThamGia.setText(tv.getNgayThamGia());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listTV.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvStt, tvMSSV , tvHoTen, tvLop, tvKhoa,tvNgayThamGia;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvStt = itemView.findViewById(R.id.tv_TV_STT);
+            tvHoTen = itemView.findViewById(R.id.tv_TV_HoTen);
+            tvMSSV = itemView.findViewById(R.id.tv_TV_HoTen);
+            tvLop = itemView.findViewById(R.id.tv_TV_Lop);
+            tvKhoa = itemView.findViewById(R.id.tv_TV_Khoa);
+            tvNgayThamGia = itemView.findViewById(R.id.tv_TV_NgayThamGia);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+        }
+
+    }
+}
